d308f50985427ac7944a6009103398ab5360017d
GottemukkalaGayathri <gayathrigottemukkala123@gmail.com>
2025-06-24 16:17:47 +0530

    backend created

--- src/pages/Auth/Signup.jsx
+++ src/pages/Auth/Signup.jsx
@@ -1,93 +1,179 @@
+
+
 import React, { useEffect, useState } from "react";
-import { FaGoogle,FaFacebook,FaLinkedin } from "react-icons/fa";
-import slide1 from "../../images/signup1.png"
-import slide2 from "../../images/signup2.png"
-import slide3 from "../../images/signup3.png"
-import "./auth.css";
+import { FaGoogle, FaFacebook, FaLinkedin } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
+import slide1 from "../../images/signup1.png";
+import slide2 from "../../images/signup2.png";
+import slide3 from "../../images/signup3.png";
+import "./auth.css";
 
-const images = [
-  slide1,slide2,slide3
-];
-
+const images = [slide1, slide2, slide3];
 
 const Signup = () => {
-   const [current, setCurrent] = useState(0);
-   const navigate=useNavigate()
-
+  const [formData, setFormData] = useState({
+    fullName: "",
+    gender: "",
+    jobStatus: "",
+    email: "",
+    password: "",
+  });
+  const [current, setCurrent] = useState(0);
+  const [message, setMessage] = useState("");
+  const [messageType, setMessageType] = useState("");
+  const navigate = useNavigate();
+    const Backendurl=process.env.REACT_APP_BACKEND_URL
+console.log(Backendurl,"Backendurl")
+  // Carousel effect
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrent((prev) => (prev + 1) % images.length);
-    }, 3000); // change slide every 3s
+    }, 3000);
     return () => clearInterval(interval);
   }, []);
-  return (
-    <div className="signup-container">
- <div className="signup-wrapper">
-  <h1>“Join  Streelancer to start your journey”</h1>
-      <div className="signup-card">
-        <form className="signup-form">
-          <input type="text" placeholder="Full name" />
-          <div className="input-row">
-            <input type="text" placeholder="Gender" />
-            <input type="text" placeholder="Current Job Status" />
-          </div>
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <button type="submit" className="signup-button">Sign up</button>
-        </form>
 
-        <div className="divider">or</div>
+  // Handle input change
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
 
-        <div className="auth-social-icons">
-          <FaGoogle/>
-          <FaFacebook/>
-          <FaLinkedin/>
-        
-        </div>
+  // Form validation
+  const isValid = () => {
+    return (
+      formData.fullName &&
+      formData.gender &&
+      formData.jobStatus &&
+      formData.email &&
+      formData.password
+    );
+  };
 
-        <p className="signin-text">Already have an account? <a onClick={() => navigate("/login")}>Sign in</a></p>
-      </div>
-    </div>
-     {/* <div className="image-carousel">
-      <img src={images[current]} alt={`Slide ${current + 1}`} className="carousel-image" />
+  // Handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    e.preventDefault();
+setMessage("");           // Clear previous message
+setMessageType("");
+    if (!isValid()) {
+      setMessage("Please fill in all fields.");
+      setMessageType("Warning")
+      return;
+    }
 
-      <div className="carousel-dots">
-        {images.map((_, index) => (
-          <span
-            key={index}
-            className={`dot ${index === current ? "active" : ""}`}
-            onClick={() => setCurrent(index)}
-          ></span>
-        ))}
-      </div>
-    </div> */}
+    try {
+      const response = await fetch(`${Backendurl}/api/signup`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
 
-    <div className="image-carousel">
-  {images.map((img, index) => (
-    <img
-      key={index}
-      src={img}
-      alt={`Slide ${index + 1}`}
-      className={`carousel-image ${index === current ? "active" : ""}`}
-    />
-  ))}
+     const data = await response.json();
 
-  <div className="carousel-dots">
-    {images.map((_, index) => (
-      <span
-        key={index}
-        className={`dot ${index === current ? "active" : ""}`}
-        onClick={() => setCurrent(index)}
-      ></span>
-    ))}
-  </div>
-</div>
-
+    if (response.ok) {
+      setMessageType("Success");
+      setMessage(data.message || "Signup successful! Redirecting...");
+      setTimeout(() => navigate("/Registration"), 2500);
+    } else {
+      setMessageType("error");
+      setMessage(data.message || "Signup failed.");
+    }
+    } catch (error) {
+      setMessage("Something went wrong. Please try again.");
+      setMessageType("error")
+    }
+  };
+  console.log(messageType,"messagetype");
   
 
+  return (
+    <div className="signup-container">
+      <div className="signup-wrapper">
+        <h1>“Join Streelancer to start your journey”</h1>
+
+        <div className="signup-card">
+          <form className="signup-form" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="fullName"
+              placeholder="Full name"
+              value={formData.fullName}
+              onChange={handleChange}
+            />
+            <div className="input-row">
+              <input
+                type="text"
+                name="gender"
+                placeholder="Gender"
+                value={formData.gender}
+                onChange={handleChange}
+              />
+              <input
+                type="text"
+                name="jobStatus"
+                placeholder="Current Job Status"
+                value={formData.jobStatus}
+                onChange={handleChange}
+              />
+            </div>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+            <button type="submit" className="signup-button">Sign up</button>
+          </form>
+
+          {message && <p style={{ color: messageType === "Success" ? "limegreen" : "#ff4d4f", marginTop: "10px" }}>{message}</p>}
+
+          <div className="divider">or</div>
+
+          <div className="auth-social-icons">
+            <FaGoogle />
+            <FaFacebook />
+            <FaLinkedin />
+          </div>
+
+          <p className="signin-text">
+            Already have an account?{" "}
+            <a onClick={() => navigate("/login")}>Sign in</a>
+          </p>
+        </div>
+      </div>
+
+      {/* Carousel */}
+      <div className="image-carousel">
+        {images.map((img, index) => (
+          <img
+            key={index}
+            src={img}
+            alt={`Slide ${index + 1}`}
+            className={`carousel-image ${index === current ? "active" : ""}`}
+          />
+        ))}
+
+        <div className="carousel-dots">
+          {images.map((_, index) => (
+            <span
+              key={index}
+              className={`dot ${index === current ? "active" : ""}`}
+              onClick={() => setCurrent(index)}
+            ></span>
+          ))}
+        </div>
+      </div>
     </div>
-   
   );
 };
 
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,4 +1,148 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
+// import "./home.css";
+// import womanLaptop from "../../images/girl.png";
+
+// import HeroServices from "./homecontent/HeroServices";
+// import CommunityBanner from "./homecontent/CommunityBanner";
+// import ExperienceBoxes from "./homecontent/ExperienceBoxes";
+// import StreelancerSteps from "./homecontent/stepscontent";
+// import { FaUserCircle } from "react-icons/fa";
+// import { useLocation, useNavigate } from "react-router-dom";
+// import { MdKeyboardArrowDown } from "react-icons/md";
+// import done from "../../images/done.png"
+
+// const HomePage = () => {
+//   const navigate = useNavigate();
+//   const [menuOpen, setMenuOpen] = useState(false);
+//    const [isLoggedIn, setIsLoggedIn] = useState(false);
+//    const { pathname } = useLocation();
+
+//   useEffect(() => {
+//     window.scrollTo({ top: 0, behavior: 'smooth' });
+//   }, [pathname]);
+
+//    useEffect(() => {
+//     const token = localStorage.getItem("token");
+//     setIsLoggedIn(!!token); // ✅ Set to true if token exists
+//   }, []);
+
+//   const toggleMenu = () => {
+//     setMenuOpen((prev) => !prev);
+//   };
+//   const handleLogout = () => {
+//     localStorage.removeItem("token"); // ✅ Clear token
+//     setIsLoggedIn(false);
+//     navigate("/login"); // Optional: Redirect to login
+//   };
+
+//   return (
+//     <div className="hero-container">
+//       {/* Navbar */}
+//       <nav className="navbar">
+//         <div className="logo">
+//           <img src={require("../../images/logo.png")} alt="Streelancer Logo" />
+//         </div>
+
+//         <ul className={`nav-links ${menuOpen ? "open" : "close"}`}>
+//           <>
+//            <li onClick={() => setMenuOpen(false)}>Home</li>
+//           <li onClick={() => setMenuOpen(false)}>Find Work <MdKeyboardArrowDown /></li>
+//           <li onClick={() => setMenuOpen(false)}>Find Talent <MdKeyboardArrowDown /></li>
+//           <li onClick={() => setMenuOpen(false)}>Why Us</li>
+//           <li onClick={() => setMenuOpen(false)}>Community</li></>
+         
+//           {/* <li onClick={() => setMenuOpen(false)}> <button className=" btn-clear" onClick={() => navigate("/Signup")}>
+//             Sign up
+//           </button></li>
+//           <li onClick={() => setMenuOpen(false)}>  <button className=" btn-clear2" onClick={() => navigate("/login")}>
+//             Log in
+//           </button></li> */}
+
+//           {isLoggedIn ? (
+//             <li onClick={() => setMenuOpen(false)}>
+//               <div
+//                 className="profile-menu"
+//                 style={{ cursor: "pointer", color: "#333" }}
+//                 onClick={handleLogout}
+//               >
+//                 <FaUserCircle size={24} /> Logout
+//               </div>
+//             </li>
+//           ) : (
+//             <>
+//               <li onClick={() => setMenuOpen(false)}>
+//                 <button className="btn-clear" onClick={() => navigate("/Signup")}>
+//                   Sign up
+//                 </button>
+//               </li>
+//               <li onClick={() => setMenuOpen(false)}>
+//                 <button className="btn-clear2" onClick={() => navigate("/login")}>
+//                   Log in
+//                 </button>
+//               </li>
+//             </>
+//           )}
+//         </ul>
+
+
+
+        
+
+//         <div className="hamburger" onClick={toggleMenu}>
+//           {menuOpen ? "✖" : "☰"}
+//         </div>
+//       </nav>
+
+     
+
+//       {/* Hero Section */}
+//       <section className="hero">
+//         <div className="hero-text">
+//           <div>
+//           <h1>Hire. Train. Empower.</h1>
+//           <p>India's largest women-first platform for inclusive career building.</p>
+//           <div className="hero-buttons">
+//             <button className="btn-primary">Register Now</button>
+//             <button className="btn-primary btn-outline ">Join Academy</button>
+//           </div>
+//           <ul className="features">
+//             <li><span><img src={done}/></span>DEI-aligned, work-from-home talent</li>
+//             <li><span><img src={done}/></span>Vetted profiles, ready to get hired</li>
+//             <li><img src={done}/>Courses + mentorship for career returnees</li>
+//           </ul>
+//         </div>
+//         </div>
+
+//         <div className="hero-image">
+//           <img src={womanLaptop} alt="Woman with laptop"/>
+//         </div>
+
+//         {/* <div className="hero-container">
+//       <div className="bg-shape"></div>
+
+//       <img src={ellipse6} alt="ellipse1" className="ellipse ellipse1" />
+//       <img src={ellipse4} alt="ellipse2" className="ellipse ellipse2" />
+//        <img src={ellipse2} alt="ellipse6" className="ellipse ellipse6" />
+//       <img src={ellipse3} alt="ellipse3" className="ellipse ellipse3" />
+//       <img src={ellipse5} alt="ellipse4" className="ellipse ellipse4" />
+//       <img src={ellipse5} alt="ellipse5" className="ellipse ellipse5" />
+
+//       <img src={girlImg} alt="Girl working" className="girl-img" />
+//     </div> */}
+//       </section>
+
+//       <HeroServices />
+//       <ExperienceBoxes />
+//       <StreelancerSteps />
+//       <CommunityBanner />
+//     </div>
+//   );
+// };
+
+// export default HomePage;
+
+
+import React, { useEffect, useState, useRef } from "react";
 import "./home.css";
 import womanLaptop from "../../images/girl.png";
 
@@ -6,21 +150,39 @@ import HeroServices from "./homecontent/HeroServices";
 import CommunityBanner from "./homecontent/CommunityBanner";
 import ExperienceBoxes from "./homecontent/ExperienceBoxes";
 import StreelancerSteps from "./homecontent/stepscontent";
-import { useLocation, useNavigate } from "react-router-dom";
 import { MdKeyboardArrowDown } from "react-icons/md";
-import done from "../../images/done.png"
+import { RiEqualizerLine } from "react-icons/ri";
+import { CiSettings } from "react-icons/ci";
+import { IoIosLogOut } from "react-icons/io";
+import done from "../../images/done.png";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const HomePage = () => {
   const navigate = useNavigate();
+  const { pathname } = useLocation();
   const [menuOpen, setMenuOpen] = useState(false);
-   const { pathname } = useLocation();
+  
+  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("token"));
+  const [showDropdown, setShowDropdown] = useState(false);
+  const dropdownRef = useRef(null);
 
   useEffect(() => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: "smooth" });
   }, [pathname]);
+
 
   const toggleMenu = () => {
     setMenuOpen((prev) => !prev);
+  };
+
+  const toggleDropdown = () => {
+    setShowDropdown((prev) => !prev);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
+    navigate("/login");
   };
 
   return (
@@ -32,60 +194,80 @@ const HomePage = () => {
         </div>
 
         <ul className={`nav-links ${menuOpen ? "open" : "close"}`}>
-          <>
-           <li onClick={() => setMenuOpen(false)}>Home</li>
-          <li onClick={() => setMenuOpen(false)}>Find Work <MdKeyboardArrowDown /></li>
-          <li onClick={() => setMenuOpen(false)}>Find Talent <MdKeyboardArrowDown /></li>
+          <li onClick={() => setMenuOpen(false)}>Home</li>
+          <li onClick={() => setMenuOpen(false)}>
+            Find Work <MdKeyboardArrowDown />
+          </li>
+          <li onClick={() => setMenuOpen(false)}>
+            Find Talent <MdKeyboardArrowDown />
+          </li>
           <li onClick={() => setMenuOpen(false)}>Why Us</li>
-          <li onClick={() => setMenuOpen(false)}>Community</li></>
-         
-          <li onClick={() => setMenuOpen(false)}> <button className=" btn-clear" onClick={() => navigate("/Signup")}>
-            Sign up
-          </button></li>
-          <li onClick={() => setMenuOpen(false)}>  <button className=" btn-clear2" onClick={() => navigate("/login")}>
-            Log in
-          </button></li>
+          <li onClick={() => setMenuOpen(false)}>Community</li>
+          <li> {isLoggedIn && (
+          <div className="profile-dropdown-container desktop-only">
+            <img
+              src={require("../../images/profile.avif")} // Replace with actual profile picture if needed
+              alt="Profile"
+              className="profile-image"
+              onClick={toggleDropdown}
+            />
+            <MdKeyboardArrowDown onClick={toggleDropdown} className="dropdown-arrow" />
+            {showDropdown && (
+              <div className="profile-dropdown" ref={dropdownRef}>
+                <div className="dropdown-item"><RiEqualizerLine/>Account</div>
+                <div className="dropdown-item"><CiSettings/>Settings</div>
+                <div className="dropdown-item" onClick={handleLogout}>
+                 <IoIosLogOut/> Log Out
+                </div>
+              </div>
+            )}
+          </div>
+        )}</li>
+
+          {isLoggedIn ? (
+            <>
+              {/* ✅ Show 3 options in mobile nav */}
+              <li onClick={() => setMenuOpen(false)} className="mobile-only"><RiEqualizerLine/>Account</li>
+              <li onClick={() => setMenuOpen(false)} className="mobile-only"><CiSettings/>Settings</li>
+              <li
+                onClick={() => {
+                  handleLogout();
+                  setMenuOpen(false);
+                }}
+                className="mobile-only"
+              >
+               <IoIosLogOut/> Log Out
+              </li>
+            </>
+          ) : (
+            <>
+              <li onClick={() => setMenuOpen(false)}>
+                <button
+                  className="btn-clear"
+                  onClick={() => navigate("/Signup")}
+                >
+                  Sign up
+                </button>
+              </li>
+              <li onClick={() => setMenuOpen(false)}>
+                <button
+                  className="btn-clear2"
+                  onClick={() => navigate("/login")}
+                >
+                  Log in
+                </button>
+              </li>
+            </>
+          )}
         </ul>
 
-        {/* <div className="nav-buttons desktop-only">
-          <button className="btn-outline" onClick={() => navigate("/register")}>
-            Sign up
-          </button>
-          <button className="btn-primary" onClick={() => navigate("/login")}>
-            Log in
-          </button>
-        </div> */}
-
-        
+   
+       
 
         <div className="hamburger" onClick={toggleMenu}>
           {menuOpen ? "✖" : "☰"}
         </div>
       </nav>
-
-      {/* Mobile Buttons */}
-      {/* {menuOpen && (
-        <div className="nav-buttons-mobile">
-          <button
-            className="btn-outline"
-            onClick={() => {
-              navigate("/register");
-              setMenuOpen(false);
-            }}
-          >
-            Sign up
-          </button>
-          <button
-            className="btn-primary"
-            onClick={() => {
-              navigate("/login");
-              setMenuOpen(false);
-            }}
-          >
-            Log in
-          </button>
-        </div>
-      )} */}
 
       {/* Hero Section */}
       <section className="hero">
@@ -95,32 +277,32 @@ const HomePage = () => {
           <p>India's largest women-first platform for inclusive career building.</p>
           <div className="hero-buttons">
             <button className="btn-primary">Register Now</button>
-            <button className="btn-primary btn-outline ">Join Academy</button>
+            <button className="btn-primary btn-outline">Join Academy</button>
           </div>
           <ul className="features">
-            <li><span><img src={done}/></span>DEI-aligned, work-from-home talent</li>
-            <li><span><img src={done}/></span>Vetted profiles, ready to get hired</li>
-            <li><img src={done}/>Courses + mentorship for career returnees</li>
+            <li>
+              <span>
+                <img src={done} />
+              </span>
+              DEI-aligned, work-from-home talent
+            </li>
+            <li>
+              <span>
+                <img src={done} />
+              </span>
+              Vetted profiles, ready to get hired
+            </li>
+            <li>
+              <img src={done} />
+              Courses + mentorship for career returnees
+            </li>
           </ul>
-        </div>
+          </div>
         </div>
 
         <div className="hero-image">
-          <img src={womanLaptop} alt="Woman with laptop"/>
+          <img src={womanLaptop} alt="Woman with laptop" />
         </div>
-
-        {/* <div className="hero-container">
-      <div className="bg-shape"></div>
-
-      <img src={ellipse6} alt="ellipse1" className="ellipse ellipse1" />
-      <img src={ellipse4} alt="ellipse2" className="ellipse ellipse2" />
-       <img src={ellipse2} alt="ellipse6" className="ellipse ellipse6" />
-      <img src={ellipse3} alt="ellipse3" className="ellipse ellipse3" />
-      <img src={ellipse5} alt="ellipse4" className="ellipse ellipse4" />
-      <img src={ellipse5} alt="ellipse5" className="ellipse ellipse5" />
-
-      <img src={girlImg} alt="Girl working" className="girl-img" />
-    </div> */}
       </section>
 
       <HeroServices />
